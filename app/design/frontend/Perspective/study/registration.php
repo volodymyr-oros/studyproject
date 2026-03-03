eebc502036a0de51eaa28073b575f83be4bb3e89
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2026-03-03 09:16:07 +0200

    STD-831: Create a Study frontend theme

---
+++ app/design/frontend/Perspective/study/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright 2026 Perspective
+ * All rights reserved.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Perspective/study', __DIR__);
