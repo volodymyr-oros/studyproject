b8643fd5565c945feb0de85fb23a62d31082179b
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2026-03-31 20:09:37 +0300

    Setup Grunt

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,12 @@
+module.exports = {
+    study: {
+        area: 'frontend',
+        name: 'PlanetaWeb/study-theme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
