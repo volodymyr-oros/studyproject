e9c0e005f702eb48b43ef2b401fa27430cbd82cf
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2026-04-27 20:40:25 +0300

    STD-852: Show Order Summary in Shipping Step of Checkout in Magento 2

--- app/code/PlanetaWeb/ExtendedCheckout/view/frontend/requirejs-config.js
+++ app/code/PlanetaWeb/ExtendedCheckout/view/frontend/requirejs-config.js
@@ -3,6 +3,9 @@ var config = {
         mixins: {
             'Magento_Checkout/js/model/checkout-data-resolver': {
                 'PlanetaWeb_ExtendedCheckout/js/model/checkout-data-resolver-mixin': true
+            },
+            'Magento_Checkout/js/view/summary/abstract-total': {
+                'PlanetaWeb_ExtendedCheckout/js/view/summary/abstract-total-mixin': true
             }
         }
     }
