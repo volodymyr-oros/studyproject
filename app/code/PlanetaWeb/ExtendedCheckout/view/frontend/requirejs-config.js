88e6d66c05819a7d0201b5695fd5052ab1e0c779
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2026-04-27 18:05:10 +0300

    STD-852:  Pre-select Default Payment Method (Cash On Delivery) at the Checkout Page

---
+++ app/code/PlanetaWeb/ExtendedCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/model/checkout-data-resolver': {
+                'PlanetaWeb_ExtendedCheckout/js/model/checkout-data-resolver-mixin': true
+            }
+        }
+    }
+};
