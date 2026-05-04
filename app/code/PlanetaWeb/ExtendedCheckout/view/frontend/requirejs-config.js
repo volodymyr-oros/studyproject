9a569ab736eb2e71bfa8e9508a9adb0a2b310eff
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2026-05-04 21:07:48 +0300

    Update Shipping Methods when Street Field Input Change on Checkout Page in Magento 2

--- app/code/PlanetaWeb/ExtendedCheckout/view/frontend/requirejs-config.js
+++ app/code/PlanetaWeb/ExtendedCheckout/view/frontend/requirejs-config.js
@@ -6,6 +6,9 @@ var config = {
             },
             'Magento_Checkout/js/view/summary/abstract-total': {
                 'PlanetaWeb_ExtendedCheckout/js/view/summary/abstract-total-mixin': true
+            },
+            'Magento_Checkout/js/model/shipping-rates-validation-rules': {
+                'PlanetaWeb_ExtendedCheckout/js/model/shipping-rates-validation-rules-mixin': true
             }
         }
     }
---
+++ app/code/PlanetaWeb/ExtendedCheckout/view/frontend/web/js/model/shipping-rates-validation-rules-mixin.js
@@ -0,0 +1,21 @@
+define([
+    'jquery',
+    'mage/utils/wrapper'
+], function ($, Wrapper) {
+    "use strict";
+
+    return function (origRules) {
+        origRules.getObservableFields = Wrapper.wrap(
+            origRules.getObservableFields,
+            function (originalAction)
+            {
+                var fields = originalAction();
+                fields.push('street');
+
+                return fields;
+            }
+        );
+        
+        return origRules;
+    };
+});
